b74c795b92e5a767cabf86a16395205ed1235ae0
wangtong <1026481205@qq.com>
2018-08-13 23:01:20 +0800

    idea-配置git-第一次提交

---
+++ src/main/java/net/health/lucene/controller/LucentController.java
@@ -0,0 +1,8 @@
+package net.health.lucene.controller;
+
+/**
+ * Created by wangtong on 2018/8/9.
+ */
+public class LucentController {
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! luceneProject!</h2>
+</body>
+</html>
